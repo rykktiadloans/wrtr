1b1e02ad4284afbc35df246825e68a97c1db9eea
rykktiadloans <datokar1705@gmail.com>
2025-02-23 17:55:43 +0100

    added actuators and added basic admin role support

--- src/main/java/com/wrtr/wrtr/core/config/DataInitializer.java
+++ src/main/java/com/wrtr/wrtr/core/config/DataInitializer.java
@@ -1,6 +1,7 @@
 package com.wrtr.wrtr.core.config;
 
 import com.wrtr.wrtr.core.service.PlaceCanvasService;
+import com.wrtr.wrtr.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -14,11 +15,22 @@ public class DataInitializer {
     @Autowired
     private PlaceCanvasService placeCanvasService;
 
+    @Autowired
+    private UserService userService;
+
     /**
      * Is run on application startup to make sure that a place canvas service exists in the database
      */
     @EventListener(ApplicationReadyEvent.class)
     public void ensurePlaceCanvasExists() {
         this.placeCanvasService.ensurePlaceCanvasExists();
+    }
+
+    /**
+     * Is run on application startup to make sure that a default admin user exists in the database
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void ensureAdminUserExists() {
+        this.userService.ensureAdminUserExists();
     }
 }
---
+++ src/main/java/com/wrtr/wrtr/core/model/builders/UserBuilder.java
@@ -0,0 +1,78 @@
+package com.wrtr.wrtr.core.model.builders;
+
+import com.wrtr.wrtr.core.config.SecurityConfig;
+import com.wrtr.wrtr.core.model.User;
+
+import java.util.ArrayList;
+
+/**
+ * A builder class for User
+ */
+public class UserBuilder {
+    private SecurityConfig securityConfig;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    /**
+     * A default initializer. Requires the security config to get the password encoder
+     * @param securityConfig Security configuration bean
+     */
+    public UserBuilder(SecurityConfig securityConfig) {
+        this.securityConfig = securityConfig;
+    }
+
+    /**
+     * Builds the final user class based on previously used methods
+     * @return Resolved user
+     */
+    public User build() {
+        var user = new User();
+        user.setUsername(this.username);
+        user.setEmail(this.email);
+        user.setRole(this.role);
+        user.setPassword(this.securityConfig.passwordEncoder().encode(this.password));
+        user.setPostList(new ArrayList<>());
+        user.setBio("");
+        user.setProfilePicture(null);
+        return user;
+    }
+
+    /**
+     * Adds the username to the user
+     * @param username User's username
+     * @return The user builder (for chaining)
+     */
+    public UserBuilder addUsername(String username) {
+        this.username = username;
+        return this;
+    }
+    /**
+     * Adds the email to the user
+     * @param email User's email
+     * @return The user builder (for chaining)
+     */
+    public UserBuilder addEmail(String email) {
+        this.email = email;
+        return this;
+    }
+    /**
+     * Adds the password to the user
+     * @param password User's password
+     * @return The user builder (for chaining)
+     */
+    public UserBuilder addPassword(String password) {
+        this.password = password;
+        return this;
+    }
+    /**
+     * Adds the role to the user
+     * @param role User's role
+     * @return The user builder (for chaining)
+     */
+    public UserBuilder addRole(String role) {
+        this.role = role;
+        return this;
+    }
+}
